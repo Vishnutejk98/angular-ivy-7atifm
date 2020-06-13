78013f5ef2a6e0e665b9be19bb3ae3c2de0d0b38
Vishnutejk98 <mailboxnumber04@gmail.com>
2020-06-13 20:13:18 +0530

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HelloWorldComponent, DateComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
